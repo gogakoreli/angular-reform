4082500e3f4bb282607d84eaaca1c5a21df39a61
Goga Koreli <gogakoreli@gmail.com>
2019-07-05 11:54:58 +0400

    demo app: add initial demo form

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      firstName: [null, Validators.required],
+    });
+  }
+
+  public submit() {
+    console.log('bla');
+  }
 }
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReformModule } from 'reform';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReformModule],
+  imports: [BrowserModule, ReactiveFormsModule, ReformModule],
   providers: [],
   bootstrap: [AppComponent],
 })
